33d9cb80ffe3ea7d3fe79b75624f6afd00ccc0c8
jhuang10123 <133@holbertonschool.com>
2017-12-19 04:04:01 +0000

    added 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_app(self):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """custom page not found msg"""
+    message = {"error": "Not found"}
+    return jsonify(message), 404
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
