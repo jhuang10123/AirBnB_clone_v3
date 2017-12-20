9edbd0062f9f8f8bbd1c66e11df3589291a9a6d3
jhuang10123 <133@holbertonschool.com>
2017-12-20 02:47:26 +0000

    adding states

---
+++ api/v1/views/states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.state import State
+from models import storage
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def getAllStates():
+    """Retrieves the list of all State objects"""
+    all_obj = storage.all('State')
+    for item in all_obj.values():
+        return item
+
+# #Retrieves a State object: GET /api/v1/states/<state_id>
+# @app.route('/states/<int:state_id>', methods=['GET'])
+# #try except 404
