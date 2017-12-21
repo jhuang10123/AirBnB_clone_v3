3f3901aa66f01adefaaa89f2901fb8bfbfa004b4
jhuang10123 <133@holbertonschool.com>
2017-12-21 01:04:26 +0000

    removed comments

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,64 +11,50 @@ from models import storage
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def getAllStates():
     """Retrieves the list of all State objects"""
+    retval = []
     all_states = storage.all('State')
     for item in all_states.values():
-        return jsonify(item.to_dict())
+        retval.append(item.to_dict())
+    return jsonify(retval)
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
 def GET_state(state_id):
     """GET State object, else raise 404"""
-    # get all State objects
-    all_states = storage.all('State')
 
-    # matching inputed state_id against all obj ids in State class
-    for item in all_states.values():
-        if item.id == state_id:
-            return jsonify(item.to_dict())
-    # if no match, return 404 error
-    abort(404)
-
-
-@app_views.route('/states/<state_id>',
-                 strict_slashes=False, methods=['DELETE'])
-def DEL_state(state_id):
-    """ delete a state object """
-    state = storage.get('State', state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
-    state.delete()
-    state.save()
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['DELETE'])
+def DEL_state(state_id):
+    """ delete a state object """
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
     return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def POST_state():
     """adds state, raise 400 if not valid json"""
-
-    # get user data
-    # get_json method parses the incoming JSON request data and returns it as
-    # a Python dictionary.
-
     post_content = request.get_json()
-    # Post_content example:
-    # {"name":"washington"}
 
-# return error if not valid json format
     if not request.is_json:
         abort(400, "Not a JSON")
 
-# return error if user input doesnt include the key 'name'
     name = post_content.get('name')
     if not name:
         abort(400, "Missing name")
 
-# create a new state with inputed user content
+
 # send in user input(key:value) to create new object
-    new_state = State(**post_content)  # reference BaseModel
+    new_state = State(**post_content)
     storage.new(new_state)
 
-# save new object
     new_state.save()
 
     return jsonify(new_state.to_dict()), 201
